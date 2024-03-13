8283b3174169b5db8e94a1099c9c5f84c4fdd0cd
Heemin Woo <heem6woo@gmail.com>
2024-03-13 14:02:58 +0900

    Feat: command_line added

---
+++ 01_spring_boot_demo/_01_spring_boot_command_line/src/test/java/com/heemcode/demo/_01_spring_boot_devtools/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heemcode.demo._01_spring_boot_devtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
