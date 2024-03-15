99c87f7cc86cbfe15ad1bde3753f80ae9deaa400
Heemin Woo <heem6woo@gmail.com>
2024-03-15 12:44:04 +0900

    "Feat: Employee RestApi using CRUD"

---
+++ 04_spring_boot_rest_crud/_02_rest_crud_employee/src/main/java/com/heem/cruddemo/CruddemoApplication.java
@@ -0,0 +1,23 @@
+package com.heem.cruddemo;
+
+import com.heem.cruddemo.dao.EmployeeDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(EmployeeDAO employeeDAO) {
+        return runner -> {
+
+
+        };
+    }
+}
