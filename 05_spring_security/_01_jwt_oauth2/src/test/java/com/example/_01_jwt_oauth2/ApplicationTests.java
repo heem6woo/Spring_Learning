57e77f7917d9a6a989e51599742ca719924f75a0
Heemin Woo <heem6woo@gmail.com>
2024-04-08 23:32:33 +0900

    Feat: JWT

---
+++ 05_spring_security/_01_jwt_oauth2/src/test/java/com/example/_01_jwt_oauth2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._01_jwt_oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
